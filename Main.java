6c9a1ea1bcd69847421106ae81dd12d2d114f230
nurtt0949 <nurtt0949@wrdsb.ca>
2020-10-08 15:54:45 +0000

    finds number in index

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *Finds users index number of integer they entered
+ * @author Tolga Nurtekin
  */
 public class Main {
 
@@ -9,7 +10,34 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    //create a Scanner
+    Scanner input = new Scanner(System.in);
+    //ask user for integers
+    System.out.println("Please enter in 10 integers to put into the array");
+
+    //declare a constant
+    final int USER_INTEGER = 10;
+
+    //create array with 10 spots
+    double[] number = new double[USER_INTEGER];
     
+    //put integers into array into for loop
+    for(int i = 0; i < number.length; i++){
+      number[i] = input.nextDouble();
+      }
+    
+     //ask user for a number to find
+     System.out.println("Please enter a number to find");
+     //declare a variable to store users number
+     int finalNum = input.nextInt();
+    //use a for loop to go through array
+    for(int i = 0; i < number.length; i++){
+     //use if statement to find index
+     if(finalNum == number[i]){
+       System.out.println(finalNum + " is located at index " + i);
+
+     }
+    }
+
   }
 }
